9e8c7eb6c095ceb410b65228dc0354a7ca586365
goran jamal <70722651+GORANJAMALKARIM@users.noreply.github.com>
2021-08-18 04:04:31 +0200

    if and else in java

---
+++ Ifelse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifelse;
+
+/**
+ *
+ * @author kurdistan
+ */
+public class Ifelse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "goran";
+
+        if (name == "goran") {
+            System.out.println("wellcome");
+        } else {
+            System.out.println("Sorry");
+        }
+    }
+
+}
